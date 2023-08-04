6f4a6ed5aad351470fedeeb9352f45b82cc59fc5
Sergio Vidales Perez <srvidales@gmail.com>
2023-08-04 16:56:47 -0700

    Wire the basics

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs= require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title?'];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.error(err);
+        }
+        console.log('Saved.');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    let data = '';
+    generateMarkdown(data);
+    writeToFile('README.md', data);
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,17 +1,29 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  console.log('renderLicenseBadge')
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  console.log('renderLicenseLink')
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  console.log('renderLicenseSection')
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  renderLicenseBadge();
+  renderLicenseLink();
+  renderLicenseSection();
   return `# ${data.title}
 
 `;
