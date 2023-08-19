6473565a089c0c4a90a16e447ad8246c712eeae5
Sergio Vidales Perez <srvidales@gmail.com>
2023-08-19 10:35:05 -0700

    Add addLicenseQuestion to index.js but needs work

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
 // TODO: Include packages needed for this application
 const generateMarkdown = require('./utils/generateMarkdown.js');
+const licenseData = require('./utils/licenseData.js');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
     {type: 'input', name:'title', message:'What is the title?'},
-    {type: 'input', name:'description', message:'What is an appropriate description?'},
-    {type: 'input', name:'installation', message:'What are the installation instructions?'},
-    {type: 'input', name:'usage', message:'What is the usage information?'},
-    {type: 'input', name:'license', message:'What is license?'},
-    {type: 'input', name:'contributing', message:'Who is contributing?'},
-    {type: 'input', name:'tests', message:'What are the tests?'},
-    {type: 'input', name:'questions', message:'What are the questions?'},
+    {type: 'editor', name:'description', message:'What is an appropriate description?'},
+    {type: 'editor', name:'installation', message:'What are the installation instructions?'},
+    {type: 'editor', name:'usage', message:'What is the usage information?'},
+    addLicenseQuestion(),
+    {type: 'editor', name:'contributing', message:'Who is contributing?'},
+    {type: 'editor', name:'tests', message:'What are the tests?'},
+    {type: 'editor', name:'questions', message:'What are the questions?'},
 ];
+
+function addLicenseQuestion() {
+    let choices = [];
+    licenseData.licenses.map((value, index, array) => {
+        let licenseIndex = {license: null, info: null, badge: null}
+        let licenseData = {};
+        array.map((license, index) => {
+            licenseIndex.license = index;
+            licenseData.brand = license.brand;
+            license.info.map((info, index) => {
+                licenseIndex.info = index;
+                licenseData.name = info.name;
+                info.badges.map((badge, index) => {
+                    licenseIndex.badge = index;
+                    if ('title' in badge) {
+                        licenseData.title = badge.title;
+                    } else {
+                        licenseData.title = null;
+                    }
+                    let name = `${licenseData.brand} - ${licenseData.name} - ${licenseData.title}`;
+                    let choice = { name, value:licenseIndex }
+                    choices.push(choice);
+                    console.log(name);
+                    console.log(choice);
+                })
+            })
+        })
+    })
+    console.log(choices);
+    return choices;
+    // return {type: 'list', name:'license', message:'What is license?', choices:[ {name:'a', value:'1'}, {name:'b', value:'2'}, {name:'c', value:'3'}]};
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,29 @@
+const licenseData = require('./licenseData');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   console.log('renderLicenseBadge')
-  return '';
+  return licenseData.licenses[3].info[0].badges[0].badge;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   console.log('renderLicenseLink')
-  return '';
+  return licenseData.licenses[3].info[3].badges[0].text;
+}
+
+function renderLicenseTitle(license) {
+  console.log('renderLicenseTitle')
+  return `: ${licenseData.licenses[3].info[3].badges[0].title}`;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   console.log('renderLicenseSection')
-  renderLicenseBadge(license);
-  renderLicenseLink(license);
-  return '';
+  return `[![License${renderLicenseTitle(license)}](${renderLicenseBadge(license)})](${renderLicenseLink(license)})`;
 }
 
 // TODO: Create a function to generate markdown for README
